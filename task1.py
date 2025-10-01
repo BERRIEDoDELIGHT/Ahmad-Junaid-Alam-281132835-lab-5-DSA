f2465aa6a190c33738890fa33ffcebd36fcd548d
Ahmad Junaid Alam <281132835@formanite.fccollege.edu.pk>
2025-10-01 16:41:11 +0500

    Add files via upload
    with main

---
+++ task1.py
@@ -0,0 +1,41 @@
+# Task 1 
+
+class Stack:
+    def __init__(self):
+        self.stack= []
+
+    
+    def Push(self, item):
+        self.stack.append(item)
+
+    def pop(self):
+        if self.isEmpty():
+            return "empty"
+        return self.stack.pop()
+    
+    def peek(self):
+        if self.isEmpty():
+            return "Stack is empty"
+        return self.stack[-1]
+
+    def isEmpty(self):
+        return len(self.stack)==0
+
+    def size(self):
+        return len(self.stack)
+
+myStack = Stack()
+
+def main():
+    myStack.Push('A')
+    myStack.Push('B')
+    myStack.Push('C')
+
+    print(myStack.peek())
+
+    print(myStack.pop())
+    print(myStack.stack)
+    print(myStack.size())
+    print(myStack.isEmpty())
+
+main()
