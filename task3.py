38e6408164f3a35273dea7f3020112d6d6f20544
Ahmad Junaid Alam <281132835@formanite.fccollege.edu.pk>
2025-10-01 16:43:47 +0500

    Add files via upload
    without main

---
+++ task3.py
@@ -0,0 +1,28 @@
+from task1 import Stack
+
+def reverse_str(st):
+    stack = Stack()
+    new = []
+    
+    for i in st:
+        stack.Push(i)
+
+    while not stack.isEmpty():
+        popped = stack.pop()
+        new.append(popped)
+
+    print("Original", st)
+    print("New reversed", new)
+    return new
+    
+
+
+# def main2():
+#     b = ['A','B','C']
+#     reverse_str(b)
+
+#     c= ['N','J','G']
+#     reverse_str(c)
+
+
+# main2()
